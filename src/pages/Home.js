2c9bfe017e6c9648eefe767e2c87ef357257c7d6
Nico Plyley <nplyley@gmail.com>
2020-08-17 02:41:03 -0700

    Added FAQs to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {JumbotronContainer} from '../containers/Jumbotron'
 import {FooterContainer} from '../containers/Footer'
+import {FaqsContainer} from '../containers/Faqs'
 
 export default function Home(){
     return (
         <>
             <JumbotronContainer />
+            <FaqsContainer />
             <FooterContainer />
         </>
     )
