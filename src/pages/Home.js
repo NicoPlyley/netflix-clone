a1ef72fe31477d1d119f3a4e4ceedc4f8c07740b
Nico Plyley <nplyley@gmail.com>
2020-08-17 16:56:41 -0700

    Added feature and header to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import {Feature, OptForm} from '../components'
 import {JumbotronContainer} from '../containers/Jumbotron'
 import {FooterContainer} from '../containers/Footer'
 import {FaqsContainer} from '../containers/Faqs'
+import {HeaderContainer} from '../containers/Header'
 
 export default function Home(){
     return (
         <>
+            <HeaderContainer>
+                <Feature>
+                    <Feature.Title>Unlimited movies, TV shows, and more.</Feature.Title>
+                    <Feature.Subtitle>Watch anywhere. Cancel anytime.</Feature.Subtitle>
+                </Feature>
+                <OptForm>
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Try 30 Days Free</OptForm.Button>
+                </OptForm>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
