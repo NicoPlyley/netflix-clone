a29ada84aade86e8ddbdccbf3a0df2bdc4c72862
Nico Plyley <nplyley@gmail.com>
2020-08-17 00:35:20 -0700

    Added footer to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import {JumbotronContainer} from '../containers/Jumbotron'
+import {FooterContainer} from '../containers/Footer'
 
 export default function Home(){
     return (
-        <JumbotronContainer />
+        <>
+            <JumbotronContainer />
+            <FooterContainer />
+        </>
     )
 }
