01da2d098278abec2551ae47df5926f25a9cb11b
Nico Plyley <nplyley@gmail.com>
2020-08-16 20:02:33 -0700

    Added global styles

--- src/global-styles.js
+++ src/global-styles.js
@@ -3,5 +3,11 @@ import {createGlobalStyle} from 'styled-components'
 export const GlobalStyles = createGlobalStyle`
     html, body {
       font-family: sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      background-color: #000000;
+      color: #333333;
+      font-size: 16px;
+      margin: 0;
     }
 `
