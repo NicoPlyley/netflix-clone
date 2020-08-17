bc196d8edc7b6635ce06492ada7d9cdf39eaeaf3
Nico Plyley <nplyley@gmail.com>
2020-08-16 19:40:52 -0700

    Added global styles and nomalize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css'
+import {GlobalStyles} from './global-styles'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <GlobalStyles />
+        <App />
+    </div>,
+    document.getElementById('root'))
