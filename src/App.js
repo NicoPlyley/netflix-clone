bb914a2345b752fc57f4eddd49e90da3df4f587e
Nico Plyley <nplyley@gmail.com>
2020-08-17 03:36:45 -0700

    Added route for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Home from './pages/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import * as ROUTES from './constants/routes'
 
 const App = () => {
     return (
-        <Home />
+        <Router>
+            <Switch>
+                <Route path={ROUTES.HOME} exact>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
